cf415164a26baa6b5db311603f106b56ddb05e5a
MathiasMantai <mmantaibusiness@gmail.com>
2024-06-26 13:13:19 +0200

    feat: - updated the file logger to properly create a directory and log files - added a method LogMsg to log messages to a log file - added options for adding timestamps to file logger log entries

--- log/file_logging/fileLogging.go
+++ log/file_logging/fileLogging.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"path/filepath"
 )
 
 type FileLogger struct {
@@ -16,15 +17,27 @@ type FileLogger struct {
 	AfterTime  string
 	KeepOldest int32
 	DirPath    string
-	LogPrefix  string
+	LogFilePrefix  string
+	LogMessagePrefix string
+	UseTimestamp bool
 }
 
-func (l *FileLogger) LogMsg(msg string) bool {
-	encodedMsg := []byte(msg)
-	writeError := os.WriteFile(l.DirPath, encodedMsg, 0644)
+/* Setter */
 
-	return writeError == nil
+func (l *FileLogger) SetTimeZone(value string) {
+	l.TimeZone = value
 }
+
+func (l *FileLogger) SetTimeLayout(value string) {
+	l.TimeLayout = value
+}
+
+func (l *FileLogger) SetUseTimestamp(value bool) {
+	l.UseTimestamp = value
+}
+
+
+/* Getter */
 
 func (l *FileLogger) LogRotate() bool {
 	files, readDirError := os.ReadDir(l.DirPath)
@@ -36,7 +49,7 @@ func (l *FileLogger) LogRotate() bool {
 	for _, file := range files {
 		fmt.Println(file.Name())
 		logName := file.Name()
-		logTime := strings.Replace(logName, l.LogPrefix, "", -1)
+		logTime := strings.Replace(logName, l.LogFilePrefix, "", -1)
 		tmpTime, parseTimeError := time.Parse("01.12.2006 13:25", logTime)
 
 		if parseTimeError != nil {
@@ -72,8 +85,87 @@ func (l *FileLogger) InitLogDir() error {
 	return nil
 }
 
+func (l *FileLogger) InitLogFile() error {
+	logFileName, logFileNameError := GetLogFileName(l.LogFilePrefix, l.TimeLayout, l.TimeZone)
+	
+	if logFileNameError != nil {
+		return fmt.Errorf("x> Log file name could not be generated: %s", logFileNameError.Error())
+	}
+
+	if strings.TrimSpace(logFileName) == "" {
+		return errors.New("x> Log file name could not be generated")
+	}
+
+	if _, fileExistsError := os.Stat(l.DirPath); os.IsNotExist(fileExistsError) {
+		return errors.New("x> Log Directory does not exist")
+	}
+
+	if _, fileExistsError := os.Stat(filepath.Join(l.DirPath, logFileName)); !os.IsNotExist(fileExistsError) {
+        return errors.New("x> Log file already exists")
+    }
+
+	if _, createFileError := os.Create(filepath.Join(l.DirPath, logFileName)); createFileError != nil {
+		return fmt.Errorf("x> Log file could not be created: %v", createFileError)
+	}
+
+	return nil
+}
+
+func (l *FileLogger) LogMsg(msg string) error {
+	formattedMsg, formatError := l.FormatFileLogMsg(msg)
+	if formatError!= nil {
+        return fmt.Errorf("x> error formatting message: %v", formatError)
+    }
+	encodedMsg := []byte(formattedMsg)
+	logFileName, logFileNameError := GetLogFileName(l.LogFilePrefix, l.TimeLayout, l.TimeZone)
+	if logFileNameError != nil {
+		return fmt.Errorf("x> undefined log file name")
+	}
+
+	logFilePath := filepath.Join(l.DirPath, logFileName)
+	
+	file, openFileError := os.OpenFile(logFilePath, os.O_APPEND, 0644)
+	if openFileError!= nil {
+        return fmt.Errorf("x> Error opening log file: %s", openFileError.Error())
+    }
+	defer file.Close()
+
+	_, writeFileError := file.Write(encodedMsg)
+	if writeFileError != nil {
+		return fmt.Errorf("x> Error writing to log file: %s", writeFileError.Error())
+	}
+
+	return nil
+}
+
 func NewFileLogger() *FileLogger {
 	var logger FileLogger
-	logger.LogPrefix = "log_"
+	logger.UseTimestamp = false
+	logger.LogFilePrefix = "log_"
 	return &logger
+}
+
+
+/* Util */
+func (l *FileLogger) FormatFileLogMsg(msg string) (string, error) {
+	
+	if strings.TrimSpace(l.TimeLayout) != "" && strings.TrimSpace(l.TimeZone) != "" {
+		timestamp, timestampError := GetCurrentTime(l.TimeZone, l.TimeLayout)
+
+		if timestampError != nil {
+			return "", timestampError
+		}
+
+		//add a timestamp
+		if l.UseTimestamp  {
+			msg = fmt.Sprintf("[%s] - %s", timestamp, msg)
+		}
+	} else {
+		return "", fmt.Errorf("x> TimeZone or TimeLayout have not been specified correctly")
+	}
+
+	//add a line break
+	msg = fmt.Sprintf("%s\n", msg)
+
+	return msg, nil
 }
--- log/file_logging/util.go
+++ log/file_logging/util.go
@@ -1,10 +1,12 @@
 package log
 
 import (
+	"fmt"
 	"time"
 )
 
 func GetCurrentTime(timeZone string, layout string) (string, error) {
+
 	location, err := time.LoadLocation(timeZone)
 	if err != nil {
 		return "", err
@@ -12,4 +14,13 @@ func GetCurrentTime(timeZone string, layout string) (string, error) {
 
 	curTime := time.Now().In(location).Format(layout)
 	return curTime, nil
+}
+
+func GetLogFileName(fileNamePrefix string, TimeLayout string, TimeZone string) (string, error) {
+	curTime, curTimeError := GetCurrentTime(TimeZone, "2006-01-02")
+	if curTimeError != nil {
+		return "", fmt.Errorf("x> error parsing the current time for creating the log file name: %s", curTimeError.Error())
+	}
+
+	return fmt.Sprintf("%s%s.log", fileNamePrefix, curTime), nil
 }
