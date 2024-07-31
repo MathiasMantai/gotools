e032a349c666a7eb5b706a57243a4ca6bf2d55ec
MathiasMantai <mmantaibusiness@gmail.com>
2024-07-31 09:40:39 +0200

    feat: added a method MigrateEmbedded for SqliteDb to make migrations from a dir which is embedded into the binary

--- db/db.go
+++ db/db.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"embed"
 )
 
 type DbConnData struct {
@@ -63,6 +64,40 @@ func (s *SqliteDb) Migrate(migrationDir string) error {
 
 	return nil
 }
+
+
+
+// run migration from an embedded directory
+//reads every migrationfile separately and executes all qureries
+func (s *SqliteDb) MigrateEmbedded(migrationDir embed.FS, dirName string) error {
+	fmt.Println("=> Running migrations...")
+	dir, readDirError := migrationDir.ReadDir("migrations")
+	if readDirError != nil {
+		log.Fatal("=> Migration dir could not be found")
+	}
+
+	for _, fileName := range dir {
+		fmt.Printf("=> Running migration: %v\n", fileName.Name())
+		var filePath string = dirName + "/" + fileName.Name()
+		file, err := migrationDir.ReadFile(filePath)
+		if err != nil {
+			fmt.Printf("x> Error executing migration %v: %v\n", fileName.Name(), err.Error())
+			return err
+		}
+
+		fmt.Println("=> " + string(file))
+
+		_, queryError := s.DbObj.Exec(string(file))
+		if queryError != nil {
+			return queryError
+		}
+	}
+
+	return nil
+}
+
+
+
 
 /* MSSQL */
 
