3d119536e8653278c963dee70ffe741dba1022fd
MathiasMantai <mmantaibusiness@gmail.com>
2025-02-17 13:37:32 +0100

    feat: - added a package osutil (package for os specific utility) - added a function 'FileExists' which checks whether a file or directory exists

---
+++ osutil/dir.go
@@ -0,0 +1,16 @@
+package osutil
+
+import (
+	"os"
+)
+
+/**
+checks whether a file or directory exists
+*/
+func FileExists(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
