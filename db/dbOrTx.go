c3c6e4b6b15504ce943bb3ed0e1d5671631e2ca5
MathiasMantai <mmantaibusiness@gmail.com>
2026-03-13 09:49:30 +0100

    feat: created an interface DbOrTx which is a standin for both sql.DB and sql.Tx

---
+++ db/dbOrTx.go
@@ -0,0 +1,13 @@
+package db
+
+import (
+	"database/sql"
+)
+
+//interface so that 
+type DBOrTx interface {
+	Exec(query string, args ...any) (sql.Result, error)
+	QueryRow(query string, args ...any) *sql.Row
+	Query(query string, args ...any) (*sql.Rows, error)
+}
+
